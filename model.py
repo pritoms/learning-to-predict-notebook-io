bdbb1b8ec9cda64fa8e7f427af134d2955f202a0
Pritom Sarker <pritoms@gmail.com>
2022-08-16 17:19:03 -0400

    Create model.py

---
+++ model.py
@@ -0,0 +1,22 @@
+import torch as th
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+
+    def forward(self, input, hidden):
+        embedded = self.embedding(input).view(1, 1, -1)
+        output = embedded
+        output, hidden = self.gru(output, hidden)
+        output = self.out(output[0])
+        return output, hidden
+
+    def initHidden(self):
+        return th.zeros(1, 1, self.hidden_size)
